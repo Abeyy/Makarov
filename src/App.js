15e5d5e9cf0f0d2cb50e7c06e70968524d2f87f5
Abeyy <abey.joy@nyu.edu>
2017-12-07 00:41:58 -0500

    Starting header work

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import GetAPI from './components/DDragon_API/GetAPI.js';
 import ChampList from './components/ChampList/ChampList.js';
+import SiteHeader from './components/SiteHeader/SiteHeader.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      	<SiteHeader />
        	<ChampList />
       </div>
     );
--- src/components/SiteHeader/SiteHeader.js
+++ src/components/SiteHeader/SiteHeader.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './SiteHeader.css';
 
 class SiteHeader extends Component {
 	constructor() {
@@ -6,6 +7,28 @@ class SiteHeader extends Component {
 	}
 
 	render() {
+		return (
+			<nav class="navbar navbar-static-top">
+  				<div class="container-fluid">
+				    <div class="navbar-header">
+				      <a class="navbar-brand" href="#">Makarov</a>
+				    </div>
 
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				      <ul class="nav navbar-nav">
+				        <li class="active"><a href="#">Champions <span class="sr-only">(current)</span></a></li>
+				        <li><a href="#">Compositions</a></li>
+				      </ul>
+				     
+				      <ul class="nav navbar-nav navbar-right">
+				        <li><a href="#">About</a></li>
+				       	<li><a href="#">Sign In</a></li>
+				      </ul>
+	    			</div>
+  			</div>
+		</nav>
+		)
 	}
 }
+
+export default SiteHeader;
